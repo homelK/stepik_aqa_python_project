366a8b7dec3f008eff2f721bf7a66d4bac020d7d
Kanstantsin Homel <homelkv@gmail.com>
2024-09-23 11:29:46 +0300

    add base method for checking element presence

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,22 @@
+from selenium.common import NoSuchElementException
+
+
 class BasePage:
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
        self.browser.get(self.url)
+
+    # как искать (css, id, xpath), что искать (строку-селектор)
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
 
