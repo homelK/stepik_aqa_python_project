56178096f78b8c84bb384c38e56fccd1ee79c314
Kanstantsin Homel <homelkv@gmail.com>
2024-09-24 16:48:16 +0300

    test structure refactoring

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-from .pages.test_product_page import ProductPage
-import pytest
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -11,66 +10,6 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_product_page()
-    product_page.add_product_to_basket()
 
-    product_name = product_page.get_product_name()
-    product_name_confirmaion = product_page.get_product_name_on_confirmation_page()
-    assert product_name == product_name_confirmaion, "Product names are different"
 
-    product_price = product_page.get_product_price()
-    product_price_confirmation = product_page.get_product_price_on_confirmation_page()
-    assert product_price == product_price_confirmation
 
-@pytest.mark.xfail
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_product_page()
-    product_page.add_product_to_basket()
-    product_page.should_not_be_success_message()
-
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_product_page()
-    product_page.should_not_be_success_message()
-
-@pytest.mark.xfail
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_product_page()
-    product_page.add_product_to_basket()
-    product_page.should_not_be_element_with_success_msg_visible()
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.open()
-    login_page.should_be_login_page()
