06d3e6659688f2d6f11eb82d45e15a96dc78a2ca
Kanstantsin Homel <homelkv@gmail.com>
2024-09-24 15:25:44 +0300

    add three more tests for test_product_page

--- pages/locators.py
+++ pages/locators.py
@@ -7,7 +7,7 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
-class ProducPageLocators:
+class ProductPageLocators:
      ADD_TO_BASKET_BTN = (By.XPATH, "//button[contains(@class, 'btn-add-to-basket')]")
      PRODUCT_PRICE = (By.XPATH, "//div[contains(@class, 'product_main')]/p[@class='price_color']")
      PRODUCT_NAME = (By.XPATH, "//div[contains(@class, 'product_main')]/h1")
--- pages/test_product_page.py
+++ pages/test_product_page.py
@@ -1,30 +1,40 @@
 from .base_page import BasePage
-from .locators import ProducPageLocators
+from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
 
     def should_be_product_page(self):
        self.is_product_description_title_present()
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.PRODUCT_NAME_CONFIRMATION), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_element_with_success_msg_visible(self):
+        assert self.is_disappeared(*ProductPageLocators.PRODUCT_NAME_CONFIRMATION), \
+            "Element with success message is presented, but should not be"
+
     def add_product_to_basket(self):
-        add_to_basket_btn = self.browser.find_element(*ProducPageLocators.ADD_TO_BASKET_BTN)
+        add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         add_to_basket_btn.click()
         self.solve_quiz_and_get_code()
 
     def is_product_description_title_present(self):
-        assert self.is_element_present(*ProducPageLocators.PRODUCT_DESCRIPTION_TITLE), "There is no product description title"
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_DESCRIPTION_TITLE), "There is no product description title"
 
     def get_product_name(self):
-        return self.browser.find_element(*ProducPageLocators.PRODUCT_NAME).text
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
 
     def get_product_name_on_confirmation_page(self):
-        return self.browser.find_element(*ProducPageLocators.PRODUCT_NAME_CONFIRMATION).text
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_CONFIRMATION).text
 
     def get_product_price(self):
-        return self.browser.find_element(*ProducPageLocators.PRODUCT_PRICE).text[1:]
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text[1:]
 
     def get_product_price_on_confirmation_page(self):
-        return self.browser.find_element(*ProducPageLocators.PRODUCT_PRICE_CONFIRMATION).text[1:]
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_CONFIRMATION).text[1:]
+
+
 
 
 
